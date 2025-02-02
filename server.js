1eb8070b60110efa319055ed9f89dfb05f48c7db
Moses-247 <asugam9@gmail.com>
2025-02-02 20:15:29 +0300

    chatm

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const path = require('path');
+const http = require('http');
 const express = require ('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running port ${PORT}`));
